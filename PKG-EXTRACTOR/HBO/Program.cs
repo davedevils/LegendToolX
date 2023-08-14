13a3b27e104add57b94fa7d50b3d23a2e9bf69e5
Dave Devil's <davedevils@yandex.com>
2023-08-15 00:04:54 +0200

    Add new HBO PKG extractor

---
+++ PKG-EXTRACTOR/HBO/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using Ionic.Zip;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Linq;
+
+public class HolyBeastUnzipper
+{
+    private static readonly int[] PasswordArray = new int[]
+    {
+        0x59, 0x182, 0x313, 0x39E, 0x308, 0x149, 0x210, 0x206, 0x96, 0x178
+    };
+    private static Dictionary<string, string> folder = new Dictionary<string, string>
+    {
+        { "effect", "effect" },
+        { "map", "map" },
+        { "data", "data" },
+        { "char", "char" },
+        { "item", "item" },
+        { "sys", "sys" },
+        { "ui", "UI" },
+        { "uimap", "UI\\Map" },
+        { "uiicon", "UI\\Icon" },
+        { "effectanimation", "effect\\animation" },
+        { "effectmodel", "effect\\model" },
+        { "effecttexture", "effect\\texture" },
+        { "itemanimation", "item\\animation" },
+        { "itemmodel", "item\\model" },
+        { "itemtexture", "item\\texture" },
+        { "mapanimation", "map\\animation" },
+        { "mapmodel", "map\\model" },
+        { "mapmodelsky", "map\\model\\sky" },
+        { "mapmodelobject", "map\\model\\object" },
+        { "maptexture", "map\\texture" },
+        { "charanimation", "char\\animation" },
+        { "charmodel", "char\\model" },
+        { "chartexture", "char\\texture" },
+        { "datadb", "Data\\db" },
+        { "sysmodel", "sys\\model" },
+        { "systexture", "sys\\texture" },
+    };
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("-----------------------------------------------------");
+        Console.WriteLine("               HBO UNPACKER BY DAVEDEVIL'S      ");
+        Console.WriteLine("-----------------------------------------------------");
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Usage: HBOUnpacker.exe <inputDirectory> <outputDirectory>");
+            Console.WriteLine("\nPress key for close this app ...");
+            Console.ReadKey();
+            return;
+        }
+
+        try
+        {
+            string inputDirectory = args[0];
+            string outputDirectory = args[1];
+
+            foreach (var file in Directory.GetFiles(inputDirectory))
+            {
+                ProcessFile(file, outputDirectory);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+
+    private static void ProcessFile(string inputFile, string outputDirectory)
+    {
+        if (!File.Exists(inputFile))
+        {
+            Console.WriteLine($"The input file {inputFile} does not exist...");
+            return;
+        }
+
+        if (!Directory.Exists(outputDirectory))
+        {
+            Directory.CreateDirectory(outputDirectory);
+        }
+
+        using (FileStream fs = new FileStream(inputFile, FileMode.Open, FileAccess.Read))
+        using (ZipFile zip = ZipFile.Read(fs))
+        {
+            foreach (ZipEntry entry in zip)
+            {
+                MemoryStream decryptedStream = new MemoryStream();
+                string password = GeneratePassword(entry.FileName);
+
+                try
+                {
+                    // Decrypt and extract the content of the file
+                    entry.ExtractWithPassword(decryptedStream, password);
+                    decryptedStream.Position = 0;
+                    string sanitizedFileName = SanitizeFileName(entry.FileName);
+                    string NewNamePath = DecideFolderForFile(sanitizedFileName);
+                    string outputPath = Path.Combine(outputDirectory, NewNamePath);
+                    EnsureDirectoryExists(outputPath);
+                    using (FileStream outFile = new FileStream(outputPath, FileMode.Create))
+                    {
+                        decryptedStream.CopyTo(outFile);
+                    }
+                    Console.WriteLine($"Extracted: {entry.FileName} at {outputPath}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Failed to extract {entry.FileName}. Error: {ex.Message}");
+                }
+            }
+        }
+        Console.WriteLine($"Extract of {inputFile} finished !");
+    }
+
+    private static string DecideFolderForFile(string filename)
+    {
+        Dictionary<string, string> folderMappings = new Dictionary<string, string>(folder);
+        if (filename.StartsWith("map"))
+        {
+            for (int i = 1; i <= 63; i++)
+            {
+                for (int j = 1; j <= 255; j++)
+                {
+                    folderMappings["mapmodel" + i.ToString("X2").ToLower() + "_" + j.ToString("X2").ToLower()] = "map\\model\\" + i.ToString("X2").ToLower() + "_" + j.ToString("X2").ToLower();
+                    folderMappings["mapmodel" + i.ToString("X2").ToLower() + "_" + j.ToString("X2").ToLower()+ "_0000"] = "map\\model\\" + i.ToString("X2").ToLower() + "_" + j.ToString("X2").ToLower() + "_0000";
+                    //folderMappings["maptexture" + i.ToString("X2").ToLower() + "_" + j.ToString("X2").ToLower()] = "map\\texture\\" + i.ToString("X2").ToLower() + "_" + j.ToString("X2").ToLower();
+                }
+            }
+        }
+
+        foreach (string prefix in folderMappings.Keys.OrderByDescending(k => k.Length).ToList())
+        {
+            if (filename.StartsWith(prefix) && (!filename.StartsWith(prefix + "_")))
+            {
+                string modifiedPath = Path.Combine(folderMappings[prefix], filename.Substring(prefix.Length));
+                return modifiedPath;
+            }
+        }
+
+        return filename;
+    }
+
+
+
+    private static string GeneratePassword(string name)
+    {
+        int nameLength = name.Length;
+        int FinalByte = 0;
+
+        char[] password = new char[nameLength];
+
+        for (int x = 0; x < nameLength; x++)
+        {
+            int XOR = x % 10;
+            int SwapedByte = PasswordArray[XOR];
+            FinalByte = (FinalByte + SwapedByte) % nameLength;
+            password[x] = name[FinalByte];
+        }
+
+        return new string(password);
+    }
+
+    // Common function used
+    private static string SanitizeFileName(string fileName)
+    {
+        var invalidChars = Path.GetInvalidFileNameChars();
+        foreach (var invalidChar in invalidChars)
+        {
+            fileName = fileName.Replace(invalidChar.ToString(), "");
+        }
+        return fileName;
+    }
+    private static void EnsureDirectoryExists(string fullPath)
+    {
+        string directoryPath = Path.GetDirectoryName(fullPath);
+        if (!Directory.Exists(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+    }
+
+}
---
+++ PKG-EXTRACTOR/HBO/ZipCrypto.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+public class ZipCrypto
+{
+    private uint[] keys = new uint[3];
+
+    private static uint DecryptByte(uint[] keys)
+    {
+        uint temp = ((keys[2] & 0xFFFF) | 2);
+        return (temp * (temp ^ 1)) >> 8;
+    }
+
+    private static void UpdateKeys(byte byteValue, uint[] keys)
+    {
+        keys[0] = Crc32.ComputeCrc32(keys[0], byteValue);
+        keys[1] += (byte)keys[0];
+        keys[1] = keys[1] * 134775813 + 1;
+        keys[2] = Crc32.ComputeCrc32(keys[2], (byte)(keys[1] >> 24));
+    }
+
+    public void Initialize(string password)
+    {
+        keys[0] = 0x12345678;
+        keys[1] = 0x23456789;
+        keys[2] = 0x34567890;
+
+        foreach (char passChar in password)
+        {
+            UpdateKeys((byte)passChar, keys);
+        }
+    }
+
+    public byte[] Decrypt(byte[] cipherText)
+    {
+        byte[] plainText = new byte[cipherText.Length];
+        for (int i = 0; i < cipherText.Length; i++)
+        {
+            byte decryptedByte = (byte)(cipherText[i] ^ DecryptByte(keys));
+            UpdateKeys(decryptedByte, keys);
+            plainText[i] = decryptedByte;
+        }
+        return plainText;
+    }
+}
+
+public static class Crc32
+{
+    private static readonly uint[] CrcTable;
+
+    static Crc32()
+    {
+        const uint polynomial = 0xedb88320;
+        CrcTable = new uint[256];
+
+        for (int i = 0; i < 256; i++)
+        {
+            uint crc = (uint)i;
+            for (uint j = 8; j > 0; j--)
+            {
+                if ((crc & 1) == 1)
+                    crc = (crc >> 1) ^ polynomial;
+                else
+                    crc >>= 1;
+            }
+            CrcTable[i] = crc;
+        }
+    }
+
+    public static uint ComputeCrc32(uint oldCrc, byte value)
+    {
+        return CrcTable[(oldCrc ^ value) & 0xFF] ^ (oldCrc >> 8);
+    }
+}
